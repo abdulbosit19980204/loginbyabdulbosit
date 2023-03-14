5418efef3960df981ada9d0e1b693e4b9dfcb9fe
decoder <abdulbosit19980204@gmail.com>
2023-03-14 20:00:48 +0500

    register done

--- main.go
+++ main.go
@@ -18,12 +18,48 @@ func main() {
 
 	// Gin server yaratish
 	router := gin.Default()
-	router.LoadHTMLFiles("login.html", "index.html")
+
+	router.LoadHTMLFiles("login.html", "index.html", "reg.html")
 	// Login formasi uchun endpoint yaratish
 	router.GET("/login", func(c *gin.Context) {
 
 		c.HTML(200, "login.html", gin.H{"message": "login.html"})
 
+	})
+
+	router.GET("/register", func(c *gin.Context) {
+		c.HTML(200, "reg.html", gin.H{"message": "register page"})
+	})
+
+	//registratsiya formasi
+	router.POST("/register", func(c *gin.Context) {
+		//registratsiya formasidan ma'lumotlarni olamiz
+		userid := c.PostForm("userid")
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		// Ma'lumotlar bazasidagi foydalanuvchilar jadvalidan foydalanuvchini tekshirish
+		// data := struct {
+		// 	userid   string `json:"id"`
+		// 	username int    `json:"username"`
+		// 	password string `json:"password`
+		// }{}
+		_, err := db.Exec("insert into users (id,username,password) values(?,?,?)", userid, username, password)
+		if err != nil {
+			c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		// id, err := result.LastInsertId()
+		// if err != nil {
+		// 	c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+		// 	return
+		// }
+
+		// Return the ID of the newly inserted row
+		// c.JSON(200, gin.H{"id": id})
+		// Foydalanuvchi aniqlandi
+		//c.JSON(200, gin.H{"message": fmt.Sprintf("Welcome, %s!", result)})
+		c.HTML(200, "login.html", gin.H{"message": "login.html"})
 	})
 
 	// Login formasi uchun endpoint yaratish
@@ -40,10 +76,10 @@ func main() {
 			c.JSON(401, gin.H{"error": "Invalid username or password"})
 			return
 		}
-
 		// Foydalanuvchi aniqlandi
 		//c.JSON(200, gin.H{"message": fmt.Sprintf("Welcome, %s!", result)})
 		c.HTML(200, "index.html", gin.H{"message": "index.html"})
+
 	})
 
 	// Serverni ishga tushurish
